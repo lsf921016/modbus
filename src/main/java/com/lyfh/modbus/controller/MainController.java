20057f33115599fd51d7ca837aa20844ed162533
林水丰 <shuifenglin216076@sohu-inc.com>
2019-05-28 19:35:31 +0800

    Initial commit

---
+++ src/main/java/com/lyfh/modbus/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.lyfh.modbus.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by LinShuifeng on 2019/5/28
+ * E-Mail: shuifenglin216076@sohu-inc.com
+ */
+@RestController
+public class MainController {
+}
